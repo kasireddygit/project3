dbea9877601dfe54148772ad27bb0d72ba8d5613
kasireddygit <kasireddy1703@gmail.com>
2023-11-21 11:08:08 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def bhargavi(request):
+    return render(request,'bhargavi.html')
+
+def roshini(request):
+    return render(request,'roshini.html')
+
+def bhargavi2(request):
+    return render(request,'bhargavi2.html')
